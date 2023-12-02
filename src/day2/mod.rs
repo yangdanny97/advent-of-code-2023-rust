a361be3283b4ee0f90b45f68d2783e898d0c8d5c
yangdanny97 <dzy4@cornell.edu>
2023-12-02 00:48:20 -0500

    day2

---
+++ src/day2/mod.rs
@@ -0,0 +1,95 @@
+fn input() -> String {
+    String::from(
+        r#"
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    "#,
+    )
+}
+
+pub fn part1() {
+    let input = input();
+    let input_str = input.as_str();
+    let mut sum = 0;
+    for line in input_str.lines() {
+        let trimmed = line.trim();
+        if trimmed.is_empty() {
+            continue;
+        }
+        let game_split = trimmed.split(':').collect::<Vec<_>>();
+        let game_number: i64 = game_split
+            .first()
+            .unwrap()
+            .split(' ')
+            .collect::<Vec<_>>()
+            .get(1)
+            .unwrap()
+            .parse()
+            .unwrap();
+        let rounds = game_split.get(1).unwrap().split(';').collect::<Vec<_>>();
+        let mut all_valid = true;
+        for round in rounds {
+            let contents = round.split(',').collect::<Vec<_>>();
+            for cubes in contents {
+                let cubes_split = cubes.trim().split(' ').collect::<Vec<_>>();
+                let count: i64 = cubes_split.first().unwrap().parse().unwrap();
+                let color = cubes_split.get(1).unwrap();
+                let valid = match *color {
+                    "red" => count <= 12,
+                    "green" => count <= 13,
+                    "blue" => count <= 14,
+                    _ => false,
+                };
+                if !valid {
+                    all_valid = false;
+                }
+            }
+        }
+        if all_valid {
+            sum += game_number;
+        }
+    }
+    println!("{}", sum)
+}
+
+pub fn part2() {
+    let input = input();
+    let input_str = input.as_str();
+    let mut total: i64 = 0;
+    for line in input_str.lines() {
+        let trimmed = line.trim();
+        if trimmed.is_empty() {
+            continue;
+        }
+        let game_split = trimmed.split(':').collect::<Vec<_>>();
+        let rounds = game_split.get(1).unwrap().split(';').collect::<Vec<_>>();
+        let mut min_red: i64 = 0;
+        let mut min_blue: i64 = 0;
+        let mut min_green: i64 = 0;
+        for round in rounds {
+            let contents = round.split(',').collect::<Vec<_>>();
+            for cubes in contents {
+                let cubes_split = cubes.trim().split(' ').collect::<Vec<_>>();
+                let count: i64 = cubes_split.first().unwrap().parse().unwrap();
+                let color = cubes_split.get(1).unwrap();
+                match *color {
+                    "red" => {
+                        min_red = min_red.max(count);
+                    }
+                    "green" => {
+                        min_green = min_green.max(count);
+                    }
+                    "blue" => {
+                        min_blue = min_blue.max(count);
+                    }
+                    _ => (),
+                };
+            }
+        }
+        total += min_red * min_blue * min_green;
+    }
+    println!("{}", total)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod day1;
-use day1::part1;
-use day1::part2;
+mod day2;
+use day2::part1;
+use day2::part2;
 
 fn main() {
     part1();
